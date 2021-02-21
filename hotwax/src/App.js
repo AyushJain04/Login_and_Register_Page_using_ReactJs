cacfa846dc61482d8d1b4c3ff793405e97dd4015
AyushJain04 <69982801+AyushJain04@users.noreply.github.com>
2021-02-21 09:47:57 +0530

    Update App.js

--- hotwax/src/App.js
+++ hotwax/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <h4>Hotwax</h4>
+          <h4>My_Page</h4>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
